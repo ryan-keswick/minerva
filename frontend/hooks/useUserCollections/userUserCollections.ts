6083edc804aa923b685797f48bfcccc1796a9bd3
Ryan Keswick <ryan@simplywallst.com>
2022-10-19 19:44:13 +1100

    Added Your Images Tab

--- frontend/hooks/useUserCollections/userUserCollections.ts
+++ frontend/hooks/useUserCollections/userUserCollections.ts
@@ -1,11 +1,11 @@
 import useSWR from 'swr';
-import type { ResponseData } from '@/pages/api/user/collections';
+import type { ResponseData } from '@/pages/api/user/collections/[userId]';
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export const useUserCollections = (userId: string) => {
   const { data, error } = useSWR<ResponseData>(
-    `/api/user/collections?userId=${userId}`,
+    `/api/user/collections/${userId}`,
     fetcher
   );
 
